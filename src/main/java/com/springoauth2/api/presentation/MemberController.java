bee5587563aa622d6f35cc3bc8ab1434093c2867
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-19 00:55:49 +0900

    feat: 회원가입 컨트롤러 구현

---
+++ src/main/java/com/springoauth2/api/presentation/MemberController.java
@@ -0,0 +1,30 @@
+package com.springoauth2.api.presentation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springoauth2.api.application.MemberService;
+import com.springoauth2.api.dto.CreateMemberRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<String> signUp(@RequestBody @Validated CreateMemberRequest createMemberRequest) {
+		memberService.signUp(createMemberRequest);
+		return ResponseEntity.ok("SUCCESS SIGN UP");
+	}
+}
