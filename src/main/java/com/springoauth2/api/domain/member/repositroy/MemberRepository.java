a045f87e7db22426d273c0353c4dcba97194309d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-19 02:47:31 +0900

    feat: 사용자 이메일 조회 로직 추가

--- src/main/java/com/springoauth2/api/domain/member/repositroy/MemberRepository.java
+++ src/main/java/com/springoauth2/api/domain/member/repositroy/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.springoauth2.api.domain.member.repositroy;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.springoauth2.api.domain.member.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findMemberByEmail(String email);
 
 	boolean existsMemberByEmail(String email);
 
