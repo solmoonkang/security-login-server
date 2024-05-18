a64a8ce98cf00ac9bf8ef44c65667290356bbf30
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-19 01:34:23 +0900

    feat: Builder 어노테이션 추가

--- src/main/java/com/springoauth2/api/dto/CreateMemberRequest.java
+++ src/main/java/com/springoauth2/api/dto/CreateMemberRequest.java
@@ -2,7 +2,9 @@ package com.springoauth2.api.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record CreateMemberRequest(
 	@Email(message = "이메일 형식에 맞도록 작성해주세요.")
 	@NotBlank(message = "이메일을 입력해주세요.")
