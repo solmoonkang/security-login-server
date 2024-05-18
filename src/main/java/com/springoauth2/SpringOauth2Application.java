9057f078af5796c981a44d917aeb1ef10b8cd895
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-19 02:47:16 +0900

    feat: 컴포넌트 스캔 어노테이션 추가

--- src/main/java/com/springoauth2/SpringOauth2Application.java
+++ src/main/java/com/springoauth2/SpringOauth2Application.java
@@ -2,8 +2,10 @@ package com.springoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringOauth2Application {
 
 	public static void main(String[] args) {
