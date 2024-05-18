108909e878a69b6f173864372846e34c67cbb4d7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-19 00:55:37 +0900

    feat: 회원가입 DTO 생성

---
+++ src/main/java/com/springoauth2/api/dto/CreateMemberRequest.java
@@ -0,0 +1,24 @@
+package com.springoauth2.api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateMemberRequest(
+	@Email(message = "이메일 형식에 맞도록 작성해주세요.")
+	@NotBlank(message = "이메일을 입력해주세요.")
+	String email,
+
+	@NotBlank(message = "닉네임을 입력해주세요.")
+	String nickname,
+
+	@NotBlank(message = "비밀번호를 입력해주세요.")
+	String password,
+
+	@NotBlank(message = "확인 비밀번호를 입력해주세요.")
+	String checkPassword,
+
+	String blog,
+
+	String introduce
+) {
+}
