932ab0a0d03884e7543627c792b9e7ef073dfabd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-19 16:49:21 +0900

    test: 사용자 엔티티 생성 테스트 코드 구현

--- src/test/java/com/springoauth2/support/MemberFixture.java
+++ src/test/java/com/springoauth2/support/MemberFixture.java
@@ -1,8 +1,15 @@
 package com.springoauth2.support;
 
+import com.springoauth2.api.domain.member.Member;
 import com.springoauth2.api.dto.CreateMemberRequest;
 
 public class MemberFixture {
+
+	public static Member createMemberEntity() {
+		return Member.createMember(
+			createMemberEntityForCreateMemberRequest(),
+			"1q2w3e4r!");
+	}
 
 	public static CreateMemberRequest createMemberRequest() {
 		return CreateMemberRequest.builder()
@@ -21,6 +28,15 @@ public class MemberFixture {
 			.nickname("solmoon")
 			.password("1q2w3e4r!")
 			.checkPassword("1q2w3e4r!wrong")
+			.blog("https://www.solmoon.com")
+			.introduce("Hello 👏")
+			.build();
+	}
+
+	public static CreateMemberRequest createMemberEntityForCreateMemberRequest() {
+		return CreateMemberRequest.builder()
+			.email("solmoon@gmail.com")
+			.nickname("solmoon")
 			.blog("https://www.solmoon.com")
 			.introduce("Hello 👏")
 			.build();
