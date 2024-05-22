84eeeeec19bcbec2cd5602322b34749fc80974ac
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-22 22:24:16 +0900

    test: 게시글 엔티티 생성 테스트 코드 작성

--- src/test/java/com/springoauth2/support/PostFixture.java
+++ src/test/java/com/springoauth2/support/PostFixture.java
@@ -1,5 +1,9 @@
 package com.springoauth2.support;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.springoauth2.api.domain.post.Post;
 import com.springoauth2.api.dto.post.CreatePostRequest;
 
 public class PostFixture {
@@ -9,5 +13,13 @@ public class PostFixture {
 			.title("Test Post Title")
 			.content("Test Post Content")
 			.build();
+	}
+
+	public static List<Post> createPostList() {
+		return Collections.singletonList(createPostEntity());
+	}
+
+	public static Post createPostEntity() {
+		return Post.createPost(MemberFixture.createMemberEntity(), createPostRequest());
 	}
 }
