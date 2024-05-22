361c7baf5ae8b8d17460445b7c3f373c9087bacd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-22 22:25:00 +0900

    feat: 회원을 통해 게시글 조회하는 로직 구현

--- src/main/java/com/springoauth2/api/domain/post/repository/PostRepository.java
+++ src/main/java/com/springoauth2/api/domain/post/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.springoauth2.api.domain.post.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.springoauth2.api.domain.member.Member;
 import com.springoauth2.api.domain.post.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+	List<Post> findPostByMember(Member member);
 }
