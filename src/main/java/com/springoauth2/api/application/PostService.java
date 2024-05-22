12a1d7ed5c0acb5dd146c357b89abc96dd70c6df
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-22 22:25:07 +0900

    feat: 게시글 목록 조회 로직 구현

--- src/main/java/com/springoauth2/api/application/PostService.java
+++ src/main/java/com/springoauth2/api/application/PostService.java
@@ -1,5 +1,7 @@
 package com.springoauth2.api.application;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +12,7 @@ import com.springoauth2.api.domain.member.repositroy.MemberRepository;
 import com.springoauth2.api.domain.post.Post;
 import com.springoauth2.api.domain.post.repository.PostRepository;
 import com.springoauth2.api.dto.post.CreatePostRequest;
+import com.springoauth2.api.dto.post.PostResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,15 +26,28 @@ public class PostService {
 
 	@Transactional
 	public void createPost(AuthMember authMember, CreatePostRequest createPostRequest) {
-		final Member member = memberRepository.findMemberByEmail(authMember.email())
-			.orElseThrow(() -> new UsernameNotFoundException("❎[ERROR] 요청하신 회원은 존재하지 않는 회원입니다."));
+		final Member member = getMemberByEmail(authMember.email());
 
-		final Post post = Post.builder()
-			.title(createPostRequest.title())
-			.content(createPostRequest.content())
-			.member(member)
-			.build();
+		final Post post = Post.createPost(member, createPostRequest);
 
 		postRepository.save(post);
+	}
+
+	public List<PostResponse> getAllPosts(AuthMember authMember) {
+		final Member member = getMemberByEmail(authMember.email());
+		final List<Post> posts = postRepository.findPostByMember(member);
+
+		return posts.stream()
+			.map(post -> convertToPostResponse(post, member))
+			.toList();
+	}
+
+	private PostResponse convertToPostResponse(Post post, Member member) {
+		return new PostResponse(post.getTitle(), post.getContent(), member.getNickname());
+	}
+
+	private Member getMemberByEmail(String email) {
+		return memberRepository.findMemberByEmail(email)
+			.orElseThrow(() -> new UsernameNotFoundException("❎[ERROR] 요청하신 회원은 존재하지 않는 회원입니다."));
 	}
 }
