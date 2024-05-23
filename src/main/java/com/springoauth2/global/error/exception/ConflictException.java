ebd26bac9dc310dc84aaa914236b3bbd790425da
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-24 01:26:00 +0900

    feat: 충돌 예외 클래스 구현

---
+++ src/main/java/com/springoauth2/global/error/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package com.springoauth2.global.error.exception;
+
+public class ConflictException extends LoginServerException {
+
+	public ConflictException(String message) {
+		super(message);
+	}
+}
