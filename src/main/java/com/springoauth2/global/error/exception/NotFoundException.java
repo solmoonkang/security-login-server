757bc50b5386563684240ecb2f8415742730317b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-24 01:26:32 +0900

    feat: 찾지 못한 예외 클래스 구현

---
+++ src/main/java/com/springoauth2/global/error/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.springoauth2.global.error.exception;
+
+public class NotFoundException extends LoginServerException {
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
