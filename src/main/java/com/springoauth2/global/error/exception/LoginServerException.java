0d6f59b06e273b5f7e0947cf00e433ca387464f4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-24 01:25:37 +0900

    feat: 커스텀 예외 클래스 구현

---
+++ src/main/java/com/springoauth2/global/error/exception/LoginServerException.java
@@ -0,0 +1,8 @@
+package com.springoauth2.global.error.exception;
+
+public class LoginServerException extends RuntimeException {
+
+	public LoginServerException(String message) {
+		super(message);
+	}
+}
