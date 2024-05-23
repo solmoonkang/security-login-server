8d214e67888878652ef498cb6502c7a74f4dd54f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-24 01:25:51 +0900

    feat: 잘못된 요청 예외 클래스 구현

---
+++ src/main/java/com/springoauth2/global/error/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.springoauth2.global.error.exception;
+
+public class BadRequestException extends LoginServerException {
+
+	public BadRequestException(String message) {
+		super(message);
+	}
+}
