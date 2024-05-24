618502e096a92a5b83f3b81bf67b332d0f347cc0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-25 00:53:15 +0900

    test: classes 속성 추가

--- src/test/java/com/springoauth2/SpringOauth2ApplicationTests.java
+++ src/test/java/com/springoauth2/SpringOauth2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.springoauth2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringOauth2ApplicationTests.class)
 class SpringOauth2ApplicationTests {
 
 	@Test
