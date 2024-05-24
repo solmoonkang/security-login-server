3f122bef9a70ed60daa523519e86c3308771c3ef
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-24 20:04:24 +0900

    refactor: 초기화 로직 변경

--- src/main/java/com/springoauth2/global/config/TokenConfig.java
+++ src/main/java/com/springoauth2/global/config/TokenConfig.java
@@ -7,23 +7,32 @@ import javax.crypto.SecretKey;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @ConfigurationProperties(prefix = "jwt")
 public class TokenConfig {
 
-	private final String iss;
-	private final String secretAccessKey;
-	private final long accessExpire;
-	private final long refreshExpire;
-	private final SecretKey secretKey;
+	private String iss;
+	private String secretAccessKey;
+	private long accessExpire;
+	private long refreshExpire;
+	private SecretKey secretKey;
 
 	public TokenConfig(String iss, long accessExpire, long refreshExpire, String secretAccessKey) {
 		this.iss = iss;
 		this.accessExpire = accessExpire;
 		this.refreshExpire = refreshExpire;
 		this.secretAccessKey = secretAccessKey;
+		initializeSecretKey();
+	}
+
+	@PostConstruct
+	private void initializeSecretKey() {
 		this.secretKey = Keys.hmacShaKeyFor(secretAccessKey.getBytes(StandardCharsets.UTF_8));
 	}
 }
