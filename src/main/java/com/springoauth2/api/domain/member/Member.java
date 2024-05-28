ab2671e04207c07506d61fb3b2727239a81b2825
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-28 23:01:26 +0900

    refactor: ID 컬럼명 수정

--- src/main/java/com/springoauth2/api/domain/member/Member.java
+++ src/main/java/com/springoauth2/api/domain/member/Member.java
@@ -21,7 +21,7 @@ public class Member {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name = "member_id")
 	private Long id;
 
 	@Column(name = "email")
