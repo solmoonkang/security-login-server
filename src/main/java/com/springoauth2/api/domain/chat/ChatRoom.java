0425503e9bf28a47ac9921714b33f049cd648778
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-29 19:17:24 +0900

    feat: 채팅방 엔티티 생성

--- src/main/java/com/springoauth2/api/domain/chat/ChatRoom.java
+++ src/main/java/com/springoauth2/api/domain/chat/ChatRoom.java
@@ -1,10 +1,19 @@
 package com.springoauth2.api.domain.chat;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.springoauth2.api.domain.gallery.Gallery;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -22,10 +31,28 @@ public class ChatRoom {
 	@Column(name = "chat_room_id")
 	private Long id;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn
+	private Gallery gallery;
+
+	@Column(name = "name")
 	private String name;
 
+	@CreatedDate
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
 	@Builder
-	private ChatRoom(String name) {
+	private ChatRoom(Gallery gallery, String name) {
+		this.gallery = gallery;
 		this.name = name;
+		this.createdAt = LocalDateTime.now();
+	}
+
+	public static ChatRoom createChatRoom(Gallery gallery, String name) {
+		return ChatRoom.builder()
+			.gallery(gallery)
+			.name(name)
+			.build();
 	}
 }
