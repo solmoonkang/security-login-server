fb94ec751b593a80478f4d90da8add078dc80968
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-29 19:18:09 +0900

    feat: 채팅메시지 응답 DTO 구현

---
+++ src/main/java/com/springoauth2/api/dto/chat/ChatMessageResponse.java
@@ -0,0 +1,13 @@
+package com.springoauth2.api.dto.chat;
+
+import java.time.LocalDateTime;
+
+public record ChatMessageResponse(
+	Long chatRoomId,
+	String senderEmail,
+	String senderNickname,
+	String content,
+	LocalDateTime sendTime
+) {
+
+}
