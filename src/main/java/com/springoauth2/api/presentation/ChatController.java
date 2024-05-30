03f0eb540faf4340732f495b06ae929a205a5e64
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-30 11:08:57 +0900

    feat: 실시간 채팅 컨트롤러 구현

--- src/main/java/com/springoauth2/api/presentation/ChatController.java
+++ src/main/java/com/springoauth2/api/presentation/ChatController.java
@@ -1,16 +1,25 @@
 package com.springoauth2.api.presentation;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springoauth2.api.application.ChatService;
 import com.springoauth2.api.dto.chat.ChatMessageRequest;
 import com.springoauth2.api.dto.chat.ChatMessageResponse;
+import com.springoauth2.api.dto.chat.ChatRoomRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,11 +28,23 @@ import lombok.RequiredArgsConstructor;
 public class ChatController {
 
 	private final ChatService chatService;
+	private final SimpMessageSendingOperations simpMessageSendingOperations;
 
-	@MessageMapping("/{chatRoomId}")
-	@SendTo("/topic/{chatRoomId}")
-	public ResponseEntity<ChatMessageResponse> sendChatMessage(@DestinationVariable Long chatRoomId, @Validated @RequestBody
-	ChatMessageRequest chatMessageRequest) {
-		return ResponseEntity.ok(chatService.createChatMessage(chatRoomId, chatMessageRequest));
+	@PostMapping("/api/chat-room")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<String> createChatRoom(@Validated @RequestBody ChatRoomRequest chatRoomRequest) {
+		chatService.createChatRoom(chatRoomRequest);
+		return ResponseEntity.ok("OK");
+	}
+
+	@MessageMapping("ws/{chatRoomId}/chat-message")
+	public void sendChatMessage(@DestinationVariable Long chatRoomId,
+		@Validated @RequestBody ChatMessageRequest chatMessageRequest) {
+		simpMessageSendingOperations.convertAndSend("/topic/ws/" + chatRoomId, chatMessageRequest.message());
+	}
+
+	@GetMapping("/api/{chatRoomId}/chat-message")
+	public ResponseEntity<List<ChatMessageResponse>> getChatMessageList(@PathVariable Long chatRoomId) {
+		return ResponseEntity.ok(chatService.getChatMessageList(chatRoomId));
 	}
 }
