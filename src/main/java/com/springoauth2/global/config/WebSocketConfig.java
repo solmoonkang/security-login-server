ca784639154e5ddd9d1a85088a9f108c72af3ed6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-31 16:59:55 +0900

    refactor: 웹소켓 설정 수정

--- src/main/java/com/springoauth2/global/config/WebSocketConfig.java
+++ src/main/java/com/springoauth2/global/config/WebSocketConfig.java
@@ -12,18 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry) {
-		// "/topic"으로 시작하는 메시지는 메시지 브로커가 관리
-		messageBrokerRegistry.enableSimpleBroker("/topic");
-		// 클라이언트가 "/app"으로 시작하는 메시지를 보낼 때, 이 메시지는 @MessageMapping으로 라우팅
-		messageBrokerRegistry.setApplicationDestinationPrefixes("/app");
+		messageBrokerRegistry.enableSimpleBroker("/sub");
+		messageBrokerRegistry.setApplicationDestinationPrefixes("/pub");
 	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-		stompEndpointRegistry.addEndpoint("/ws")
-			.setAllowedOriginPatterns("*")
-			.withSockJS();
-
 		stompEndpointRegistry.addEndpoint("/ws")
 			.setAllowedOriginPatterns("*");
 	}
