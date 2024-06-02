1bb7b2d86135b06239cf7ccfb31152ad64c37c53
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-02 21:07:58 +0900

    feat: 웹소켓 연결 및 채팅방에 참여한 사용자들의 정보를 조회하는 컨트롤러 구현

--- src/main/java/com/springoauth2/api/presentation/ChatController.java
+++ src/main/java/com/springoauth2/api/presentation/ChatController.java
@@ -1,11 +1,14 @@
 package com.springoauth2.api.presentation;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +19,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springoauth2.api.application.ChatService;
+import com.springoauth2.api.domain.auth.AuthMember;
 import com.springoauth2.api.dto.chat.ChatMessageRequest;
 import com.springoauth2.api.dto.chat.ChatMessageResponse;
 import com.springoauth2.api.dto.chat.ChatRoomRequest;
-import com.springoauth2.api.dto.member.MemberResponse;
+import com.springoauth2.api.infrastructure.WebSocketEventListener;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,6 +33,7 @@ public class ChatController {
 
 	private final ChatService chatService;
 	private final SimpMessageSendingOperations simpMessageSendingOperations;
+	private final WebSocketEventListener webSocketEventListener;
 
 	@PostMapping("/api/chat-rooms")
 	@ResponseStatus(HttpStatus.CREATED)
@@ -39,19 +44,21 @@ public class ChatController {
 
 	@MessageMapping(value = "/ws/{chatRoomId}/chat-messages")
 	public void saveAndSendChatMessage(@DestinationVariable Long chatRoomId,
-		@Validated @RequestBody ChatMessageRequest chatMessageRequest
+		@Payload ChatMessageRequest chatMessageRequest,
+		SimpMessageHeaderAccessor simpMessageHeaderAccessor
 	) {
-		chatService.saveAndSendChatMessage(chatRoomId, chatMessageRequest);
+		AuthMember authMember = (AuthMember)simpMessageHeaderAccessor.getSessionAttributes().get("authMember");
+		chatService.saveAndSendChatMessage(chatRoomId, authMember, chatMessageRequest);
 		simpMessageSendingOperations.convertAndSend("/sub/ws/" + chatRoomId, chatMessageRequest.message());
+	}
+
+	@GetMapping("/api/chat-rooms/{chatRoomId}")
+	public ResponseEntity<Set<String>> getLoggedInVisitors(@PathVariable Long chatRoomId) {
+		return ResponseEntity.ok(webSocketEventListener.getActiveMembers(chatRoomId));
 	}
 
 	@GetMapping("/api/{chatRoomId}/chat-messages")
 	public ResponseEntity<List<ChatMessageResponse>> getChatMessageList(@PathVariable Long chatRoomId) {
 		return ResponseEntity.ok(chatService.getChatMessageList(chatRoomId));
-	}
-
-	@GetMapping("/api/chat-rooms/{chatRoomId}")
-	public ResponseEntity<List<MemberResponse>> getLoggedInVisitors(@PathVariable Long chatRoomId) {
-		return ResponseEntity.ok(chatService.getLoggedInVisitors(chatRoomId));
 	}
 }
