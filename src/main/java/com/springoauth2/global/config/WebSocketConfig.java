5f370b0b1ee7e004b81a8de95ca57f7da64f6eb8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-02 21:06:46 +0900

    feat: JWT 인증 및 인가 핸들러 적용

--- src/main/java/com/springoauth2/global/config/WebSocketConfig.java
+++ src/main/java/com/springoauth2/global/config/WebSocketConfig.java
@@ -1,14 +1,28 @@
 package com.springoauth2.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.springoauth2.api.application.auth.JwtProviderService;
+import com.springoauth2.api.infrastructure.AuthHandshakeInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	private final JwtProviderService jwtProviderService;
+
+	@Bean
+	public AuthHandshakeInterceptor authHandshakeInterceptor() {
+		return new AuthHandshakeInterceptor(jwtProviderService);
+	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry) {
@@ -19,6 +33,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
 		stompEndpointRegistry.addEndpoint("/ws")
+			.addInterceptors(authHandshakeInterceptor())
 			.setAllowedOriginPatterns("*");
 	}
 }
