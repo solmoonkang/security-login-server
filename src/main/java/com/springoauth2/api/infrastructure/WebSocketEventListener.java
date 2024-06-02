2ab8bc1bdab053212e1bae272fe3a5c9f07c8037
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-02 21:07:04 +0900

    feat: 웹 소켓 연결 시 동작할 이벤트 리스너 구현

--- src/main/java/com/springoauth2/api/infrastructure/WebSocketEventListener.java
+++ src/main/java/com/springoauth2/api/infrastructure/WebSocketEventListener.java
@@ -22,9 +22,22 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class WebSocketEventListener {
 
+	/**
+	 * WebSocketEventListener
+	 * STOMP 프로토콜을 사용하여 웹소켓 통신을 처리하는 리스너로, 실시간 채팅 기능을 관리하는 데 사용된다.
+	 * 이 클래스는 특히 채팅방의 구독 및 구독 해지 이벤트를 처리하고, 채팅방의 활성 사용자 목록을 유지 관리하는 역할을 한다.
+	 */
+
+	// 세션 ID와 사용자 닉네임을 매핑하는 ConcurrentHashMap. 웹소켓 세션과 사용자를 연결하는 데 사용
 	private final Map<String, String> sessionIdToNickname = new ConcurrentHashMap<>();
+
+	// 채팅방 ID와 해당 채팅방에 있는 사용자 닉네임 목록을 매핑하는 ConcurrentHashMap. 각 채팅방에 현재 접속해 있는 사용자 목록을 관리
 	private final Map<String, Set<String>> chatRoomMembers = new ConcurrentHashMap<>();
 
+	/**
+	 * handleSubscribeEvent: 사용자가 특정 채팅방에 구독할 때 호출
+	 *
+	 */
 	@EventListener
 	public void handleSubscribeEvent(SessionSubscribeEvent sessionSubscribeEvent) {
 		String sessionId = Objects.requireNonNull(
@@ -66,10 +79,20 @@ public class WebSocketEventListener {
 		}
 	}
 
+	/**
+	 * addMemberToChatRoom: 특정 사용자를 지정된 채팅방에 추가
+	 * chatRoomMembers 맵에 주어진 닉네임을 추가하여 지정된 채팅방의 사용자 목록을 갱신
+	 * 특정 상황에서 프로그래밍적으로 사용자를 채팅방에 추가하는 기능을 제공
+	 */
 	public void addMemberToChatRoom(Long chatRoomId, String nickname) {
 		chatRoomMembers.computeIfAbsent(chatRoomId.toString(), member -> new HashSet<>()).add(nickname);
 	}
 
+	/**
+	 * getActiveMembers: 지정된 채팅방의 현재 활성 사용자 목록을 반환
+	 * 주어진 채팅방 ID에 대해 활성 사용자 목록을 조회
+	 * 현재 채팅방에 접속해 있는 사용자 목록을 다른 부분에서 쉽게 조회
+	 */
 	public Set<String> getActiveMembers(Long chatRoomId) {
 		return chatRoomMembers.getOrDefault(chatRoomId.toString(), new HashSet<>());
 	}
