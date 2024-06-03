cd46e79abefd86c703009283a4ea21d53f967fbd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-03 16:18:41 +0900

    refactor: 불필요한 필드 제거

--- src/main/java/com/springoauth2/api/dto/chat/ChatMessageRequest.java
+++ src/main/java/com/springoauth2/api/dto/chat/ChatMessageRequest.java
@@ -4,9 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ChatMessageRequest(
-	@NotBlank(message = "[❎ ERROR] 이메일을 입력해주세요.")
-	String email,
-
 	@NotBlank(message = "[❎ ERROR] 메시지 내용을 입력해주세요.")
 	@Size(max = 50)
 	String message
