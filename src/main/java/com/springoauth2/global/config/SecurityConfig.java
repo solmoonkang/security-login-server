6ea33891cb69697b2b99f41da4b3e28c91ffc3ca
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-04 12:40:19 +0900

    feat: 실시간 접속 사용자 정보 조회 엔드포인트 추가

--- src/main/java/com/springoauth2/global/config/SecurityConfig.java
+++ src/main/java/com/springoauth2/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -58,6 +59,7 @@ public class SecurityConfig {
 			.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
 
 		httpSecurity.authorizeHttpRequests((auth) -> auth
+			.requestMatchers(HttpMethod.GET, "/api/chat-rooms/**").permitAll()
 			.anyRequest().authenticated()
 		);
 
